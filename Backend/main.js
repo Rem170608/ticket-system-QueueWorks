f9b9bcfa41fd805fe9a00d3cd666b49de79be7a1
Leon <leon.hebeisen08@gmail.com>
2025-10-07 14:18:02 +0200

    uihjasdasdhjklb

--- Backend/main.js
+++ Backend/main.js
@@ -48,6 +48,12 @@ app.get('/tickets', (req, res) => {
     });
 });
 
+app.get('/api/tickets', async (req, res) => {
+    const {LJ} = req.query;
+    const tickets = await db.getTickets(LJ);
+    res.json(tickets);
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
