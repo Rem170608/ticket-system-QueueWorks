26c84fe1f257d672d20edaed3b62591f677fd6fd
raffa <rem_1706@proton.me>
2025-10-08 11:05:16 +0200

    [Add] Bad actor prevention improved

--- Frontend/tracker.js
+++ Frontend/tracker.js
@@ -1,9 +1,9 @@
-        async function getIP() {
-            try {
-                const response = await fetch('https://api.ipify.org?format=json');
-                const data = await response.json();
-                document.getElementById('ip').innerText = `Remember we have your IP: ${data.ip}`;
-            } catch (error) {
-                console.error('Error fetching IP address:', error);
-            }
-        }
+async function getIP() {
+    try {
+        const response = await fetch('https://api.ipify.org?format=json');
+        const data = await response.json();
+        document.getElementById('ip').innerText = `Logged IP: ${data.ip}`;
+    } catch (error) {
+        console.error('Error fetching IP address:', error);
+    }
+}
